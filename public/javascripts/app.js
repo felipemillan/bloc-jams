4f5626d6cae9777cd0eaf1f58964dff1020cd59b
Felipe Millan <felipe@lanuu.co>
2015-02-02 15:03:29 +0000

    Converted Bloc Jams to a single page application-final

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -269,51 +269,50 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-  //require('./landing');
-  //require('./album');
-  //require('./collection');
-  //require('./profile');
-   
 blocJams = angular.module('BlocJams', ['ui.router']);
-blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+ 
+ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
    $locationProvider.html5Mode(true);
  
-<<<<<<< HEAD
-angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
-  $scope.titleText = "Bloc Jams";
-  $scope.subText = "Turn the music up!";
-=======
    $stateProvider.state('landing', {
      url: '/',
      controller: 'Landing.controller',
      templateUrl: '/templates/landing.html'
-   });
+  })
+
+    .state('song', {
+      url: '/song',
+      controller: 'song.controller',
+      templateUrl: '/templates/song.html'
+  });
  }]);
 
- blocJams.controller('Landing.controller', ['$scope', function($scope) {
+  blocJams.controller('Landing.controller', ['$scope', function($scope) {
+   $scope.title = "Bloc Jams";
    $scope.subText = "Turn the music up!";
->>>>>>> bloc-jams-spa
  
- $scope.subTextClicked = function() {
-    $scope.subText += '!';
-  };
-
-   
+   $scope.subTextClicked = function() {
+      $scope.subText += '!';
+    };
+ 
    $scope.albumURLs = [
-     '/images/album-placeholders/album-1.jpg',
-     '/images/album-placeholders/album-2.jpg',
-     '/images/album-placeholders/album-3.jpg',
-     '/images/album-placeholders/album-4.jpg',
-     '/images/album-placeholders/album-5.jpg',
-     '/images/album-placeholders/album-6.jpg',
-     '/images/album-placeholders/album-7.jpg',
-     '/images/album-placeholders/album-8.jpg',
-     '/images/album-placeholders/album-9.jpg',
-   ];
-   
-   
-   
-}]);
+      '/images/album-placeholders/album-1.jpg',
+      '/images/album-placeholders/album-2.jpg',
+      '/images/album-placeholders/album-3.jpg',
+      '/images/album-placeholders/album-4.jpg',
+      '/images/album-placeholders/album-5.jpg',
+      '/images/album-placeholders/album-6.jpg',
+      '/images/album-placeholders/album-7.jpg',
+      '/images/album-placeholders/album-8.jpg',
+      '/images/album-placeholders/album-9.jpg',
+    ];
+ 
+    $scope.shuffle = function(albumURLs) { //v1.0
+     for(var j, x, i = $scope.albumURLs.length; i; j = Math.floor(Math.random() * i), x = $scope.albumURLs[--i], $scope.albumURLs[i] = $scope.albumURLs[j], $scope.albumURLs[j] = x);
+     return $scope.albumURLs;
+     };
+     
+     }]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
